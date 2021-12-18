7992ae786f6df34a10115436ce35eb756aa08485
spxrtzy <93079460+spxrtzy@users.noreply.github.com>
2021-12-18 20:50:03 +0100

    Update encrypter.py

--- Cryptography github/V.1.0/encrypter.py
+++ Cryptography github/V.1.0/encrypter.py
@@ -4,12 +4,12 @@ file = open("key.key", "rb")
 key = file.read()
 file.close()
 
-with open("img4.jpg", "rb") as f:
+with open("test.txt", "rb") as f:
     data = f.read()
 
 fernet = Fernet(key)
 encrypted = fernet.encrypt(data)
 
 #Create the encrypted file
-with open("img4.jpg.EN", "wb") as f:
+with open("test.txt.EN", "wb") as f:
     f.write(encrypted)
